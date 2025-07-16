af719a4d695c89ea221f4e66e842bd241df9d994
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-07-17 00:39:02 +0530

    added refresh and access token methods

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, , protected: protectedRoute } = require('../controllers/authController');
+const { register, login, protected: protectedRoute } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
